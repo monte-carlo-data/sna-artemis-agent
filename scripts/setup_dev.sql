aabeae087f89a0a817b994322e098864bb4b5447
Martin Rostan <mrostan@montecarlodata.com>
2024-10-29 19:17:51 -0300

    updated scripts to run after the application is configured, removed files no longer used

--- scripts/setup_dev.sql
+++
@@ -1,8 +0,0 @@
--- run after setup_user.sql
-USE ROLE mc_app_role;
-USE DATABASE mc_app_data;
-
-CREATE SCHEMA IF NOT EXISTS mc_app;
-CREATE IMAGE REPOSITORY IF NOT EXISTS mc_app_repository;
-CREATE STAGE IF NOT EXISTS mc_app_stage
-  DIRECTORY = ( ENABLE = true );
