08e59be93d488d0b6997eee9a05816c36e16a53e
Martin Rostan <mrostan@montecarlodata.com>
2024-10-31 17:17:09 -0300

    updated mcd_app to mcd_agent

--- app/scripts/setup_secrets.sql
+++ app/scripts/setup_secrets.sql
@@ -1,4 +1,4 @@
 -- secret used to store the token used to connect to Monte Carlo, updated by the Streamlit app
-CREATE SECRET IF NOT EXISTS core.mcd_app_token
+CREATE SECRET IF NOT EXISTS core.mcd_agent_token
     TYPE=generic_string
     SECRET_STRING='{}';
