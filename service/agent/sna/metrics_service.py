c0693ad77bc5dd7d1ff0b7e9b21acef8a1631d14
Martin Rostan <mrostan@montecarlodata.com>
2024-11-14 11:08:28 -0300

    collect metrics from all nodes in the compute pool

--- service/agent/sna/metrics_service.py
+++ service/agent/sna/metrics_service.py
@@ -1,6 +1,10 @@
+import logging
+import socket
 from typing import List
 
 import requests
+
+logger = logging.getLogger(__name__)
 
 
 class MetricsService:
@@ -10,8 +14,16 @@ class MetricsService:
         Fetches metrics using Snowpark Monitoring Services:
         https://docs.snowflake.com/en/developer-guide/snowpark-container-services/monitoring-services#accessing-compute-pool-metrics
         """
-        response = requests.get(
-            "http://discover.monitor.mcd_agent_compute_pool.snowflakecomputing.internal:9001/metrics"
+        discover_host_name = (
+            "discover.monitor.mcd_agent_compute_pool.snowflakecomputing.internal"
         )
-        lines = response.text.splitlines()
+        lookup_result = socket.getaddrinfo(discover_host_name, 80)
+        addresses = set([addr[4][0] for addr in lookup_result])
+
+        logger.info(f"{discover_host_name} resolves to: {addresses}")
+        lines = []
+        for address in addresses:
+            logger.info(f"Requesting metrics from {address}")
+            response = requests.get(f"http://{address}:9001/metrics")
+            lines.extend(response.text.splitlines())
         return lines
